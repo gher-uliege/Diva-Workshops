018a6176c40caff9a58cfe49664b97cccbf1770a
Charles Troupin <charles.troupin@gmail.com>
2024-01-16 14:46:00 +0100

    configuration file with file paths

---
+++ notebooks/config.jl
@@ -0,0 +1,32 @@
+datadir = "../data"
+isdir(datadir) ? @debug("Directory already exists") : mkpath(datadir)
+
+function make_dox_url(fileid::AbstractString)
+    doxurl = "https://dox.uliege.be/index.php/s/$(fileid)/download"
+    return doxurl::String
+end
+
+function download_check(datafile::AbstractString, datafileURL::AbstractString)
+
+    if !isfile(datafile)    
+        download(datafileURL, datafile)
+    else
+        @info "File already downloaded"
+    end
+end
+
+
+salinityprovencalfile = joinpath(datadir, "WOD-Salinity-Provencal.nc")
+salinityprovencalfileURL = make_dox_url("PztJfSEnc8Cr3XN")
+salinitybigfile = joinpath(datadir, "Salinity.bigfile")
+salinitybigfileURL = make_dox_url("k0f7FxA7l5FIgu9")
+
+gebco04file = joinpath(datadir, "gebco_30sec_4.nc")
+gebco04fileURL = make_dox_url("RSwm4HPHImdZoQP")
+gebco08file = joinpath(datadir, "gebco_30sec_8.nc")
+gebco08fileURL = make_dox_url("U0pqyXhcQrXjEUX")
+gebco16file = joinpath(datadir, "gebco_30sec_16.nc")
+gebco16fileURL = make_dox_url("U0pqyXhcQrXjEUX")
+
+adriaticfile = joinpath(datadir, "AdriaticSea_SDC.nc")
+adriaticfileURL = make_dox_url("IRYJyNZ5KuKVoQL")
